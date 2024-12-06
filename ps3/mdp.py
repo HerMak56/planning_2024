1abcd82ac970ea4fae55d716b1c0bbd58b0efe14
HerMak <gdevcic10@gmail.com>
2024-12-06 22:56:44 +0300

    PS3

--- ps3/mdp.py
+++ ps3/mdp.py
@@ -24,6 +24,31 @@ class MDP:
         This function uses the Value Iteration algorithm to fill in the
         optimal value function
         """
+        max_iters = 100
+        for iter in range(max_iters):
+            for i in range(self._env.shape[0]):
+                for j in range(self._env.shape[1]):
+                    state = (i, j)
+                    if not self._env.state_consistency_check(state) or state == self._goal:
+                        continue  # skip states with collision and goal state
+                
+                    max_value = float('-inf')
+                    for action in action_space:
+                        state_propagated_list, prob_list = self._env.probabilistic_transition_function(state, action)
+                        value = 0
+
+                        for next_state, prob in zip(state_propagated_list, prob_list):
+                            if not self._env.state_consistency_check(next_state):
+                                continue
+                            reward = -1 if not self._env.state_consistency_check(next_state) else 0
+                            if next_state == self._goal:
+                                reward = 1
+                            value += prob * (reward + self._gamma * self._V[next_state])
+
+                        max_value = max(max_value, value)
+
+                    self._V[state] = max_value
+            
         return self._V
 
     def calculate_policy(self):
@@ -33,6 +58,35 @@ class MDP:
         output:
         policy: a map from each state s to the greedy best action a to execute
         """
+
+        for i in range(self._env.shape[0]):
+            for j in range(self._env.shape[1]):
+                state = (i, j)
+                if not self._env.state_consistency_check(state) or state == self._goal:
+                    continue 
+
+                best_action = None
+                max_value = -np.inf
+
+                for index,action in enumerate(action_space):
+                    state_propagated_list, prob_list = self._env.probabilistic_transition_function(state, action)
+                    value = 0
+
+                    for next_state, prob in zip(state_propagated_list, prob_list):
+                        if not self._env.state_consistency_check(next_state):
+                            continue
+
+                        reward = -1 if not self._env.state_consistency_check(next_state) else 0
+                        if next_state == self._goal:
+                            reward = 1
+                        value += prob * (reward + self._gamma * self._V[next_state])
+
+                    if value > max_value:
+                        max_value = value
+                        best_action = index
+
+                self._policy[state] = best_action 
+
         return self._policy
 
     def policy(self,state:tuple) -> int:
--- ps3/vi.py
+++ ps3/vi.py
@@ -13,6 +13,7 @@ class VI:
         self._env = env
         self._goal = goal
         self._G = np.ones(self._env.shape)*1e2
+        # self._G = np.ones(self._env.shape)* np.inf
         self._policy = np.zeros(self._env.shape, 'b') #type byte (or numpy.int8)
 
 
@@ -20,11 +21,30 @@ class VI:
         """
         env is the grid enviroment
         goal is the goal state
-            
+
         output:
         G: Optimal cost-to-go
         """
-        
+        max_iters = 100
+
+        self._G[self._goal] = 0
+
+        for iter in range(max_iters):
+            for i in range(self._env.shape[0]):
+                for j in range(self._env.shape[1]):
+
+                    state = (i,j)
+                    if not self._env.state_consistency_check(state) or state == self._goal:
+                        continue # skip states with collision and goal state
+                    costs_for_state = []
+                    for action in action_space:
+                        new_state, status = self._env.transition_function(s=state,a=action)
+                        if status:
+                            costs_for_state.append(1 + self._G[new_state])
+
+                    if costs_for_state:
+                        self._G[state] = min(costs_for_state)
+
         return self._G
         
     def calculate_policy(self):
@@ -33,7 +53,23 @@ class VI:
         
         output:
         policy: a map from each state x to the best action a to execcute
-        """    
+        """
+        for i in range(self._env.shape[0]):
+            for j in range(self._env.shape[1]):
+                state = (i,j)
+                if not self._env.state_consistency_check(state) or state == self._goal:
+                        continue # skip states with collision and goal state
+                optimal_action = None
+                min_cost = np.inf
+                for index, action in enumerate(action_space):
+                        new_state, status = self._env.transition_function(s=state,a=action)
+                        if status:
+                            cost = 1+ self._G[new_state]
+                            if cost < min_cost:
+                                min_cost = cost
+                                optimal_action = index
+                if optimal_action is not None:
+                    self._policy[state] = optimal_action
         return self._policy
         
     def policy(self,state:tuple) -> int:
